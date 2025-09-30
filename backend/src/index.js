4ff0eef591f93da6a7e6cd36919de655c3446279
Dhyanesh18 <dhyaneshvark1805@gmail.com>
2025-09-30 12:40:50 +0530

    Added routes for user operations

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
 
@@ -10,8 +11,12 @@ const app = express();
 
 const PORT = process.env.PORT
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
     console.log("Server is running on PORT:" + PORT);
+    connectDB();
 })
